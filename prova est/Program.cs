59eb14cb74c279a604d9d905d7c9c7687c2ac52d
GabriellllllllllB <164575563+GabriellllllllllB@users.noreply.github.com>
2025-06-25 15:19:39 -0300

    Add files via upload

---
+++ prova est/Program.cs
@@ -0,0 +1,68 @@
+﻿//int[] numeros = { 5, 1, 9, 4, 6 };
+//Array.Sort(numeros);
+//foreach (int num in numeros)
+//    Console.WriteLine(num);
+
+
+
+//int[] numeros = new int[10];
+//int pares = 0;
+
+//for(int i= 0; i <10; i++)
+//{
+//    Console.WriteLine("Digite: ");
+//    numeros[i]= int.Parse(Console.ReadLine());
+//    if (numeros[i] % 2 == 0)     
+// pares++;
+//}
+//Console.WriteLine($"Pares: {pares}");
+
+
+
+//List<string> nomes = new List<string>();
+//string nome;
+
+//do
+//{
+//    Console.WriteLine("Digite um nome: ");
+//    nome = Console.ReadLine();
+//    if (string.IsNullOrWhiteSpace(nome))
+//        nomes.Add(nome);
+//} while (nome != "fim");
+
+
+
+
+
+    Stack<string> pilha = new Stack<string>();
+Stack<string> frente = new Stack<string>();
+
+pilha.Push("Página 1");
+
+while (true)
+{
+    Console.WriteLine($"Página atual: {pilha.Peek()}");
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("1- Avançar");
+    Console.WriteLine("2- Voltar");
+    int opc = int.Parse(Console.ReadLine());
+
+    if (opc == 1)
+    {    Console.Write("Digite o nome da nova página: ");
+            string novaPagina = Console.ReadLine();
+            pilha.Push(novaPagina);
+            frente.Clear();
+        
+    }
+    else if (opc == 2)
+    {
+        if (pilha.Count > 1)
+        {
+            frente.Push(pilha.Pop());
+        }
+        else
+        {
+            Console.WriteLine("Você está na primeira página. Não é possível voltar.");
+        }
+    }
+}
---
+++ provaa/Program.cs
@@ -0,0 +1,87 @@
+﻿
+
+//Stack<int> pilha = new Stack<int>();
+//pilha.Push(3);
+//pilha.Push(5);
+//Console.WriteLine(pilha.Peek());
+
+
+
+
+
+//int[] v = { 1, 3, 5, 7, 9 };
+//int ini = 0, fim = v.Length - 1;
+//int meio;
+//int alvo = 5;
+//bool achou = false;
+
+//while (ini <= fim)
+//{
+//    meio = (ini + fim) / 2;
+//    if (v[meio] == alvo)
+//    {
+//        achou = true;
+//        break;
+//    }
+//    else if (alvo < v[meio])
+//    {
+//        fim = meio - 1;
+//    }
+//    else
+//    {
+//        ini = meio + 1;
+//    }
+//}
+//Console.WriteLine(achou ? "Encontrado" : "não encontrado");
+
+
+
+//int[] numeros = new int[10];
+//int pares = 0;
+
+//for (int i = 0; i <10; i++)
+//{
+//    Console.WriteLine("Digite: ");
+//    numeros[i] = int.Parse(Console.ReadLine());
+//    if ()
+//        pares++;
+//}
+
+
+//List<string> nomes = new List<string>();
+//string nome;
+
+//do
+//{
+//    Console.WriteLine("Digite um nome: ");
+//    nome = Console.ReadLine();
+//    if (nome = )
+//        nomes.Add(nome);
+//} while (nome != "fim");
+
+
+//int[] v = { 2, 4, 6 };
+
+//Console.WriteLine(v);
+
+
+int[,] matriz = new int[2, 2];
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        Console.Write($"Digite o número [{i},{j}]: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Matriz transposta:");
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        Console.Write(matriz[j, i] + " ");
+    }
+    Console.WriteLine();
+}
